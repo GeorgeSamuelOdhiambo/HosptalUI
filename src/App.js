cd3f2a84f30efa6ffb28531712dff67638b75fb1
George <georgeodhi98@gmail.com>
2021-09-01 15:26:07 +0300

    Samuel

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Dashboard from './Components/Workers/Dashbord';
+import './App.css';
+import Login from "./Components/Login"
+
+function App() {
+  return (
+    <Dashboard/>
+  );
+}
+
+export default App;
---
+++ src/Components/Workers/Orders.js
@@ -0,0 +1,69 @@
+import { React, useEffect, useState } from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Title from "./Title";
+
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+const useStyles = makeStyles((theme) => ({
+  seeMore: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+
+export default function Orders() {
+  const [rows, setrows] = useState([]);
+  const classes = useStyles();
+  const setdata = async () => {
+    const responce = await fetch("http://localhost:8080/order",{
+      method: "GET"
+    })
+    const data = await responce.json()
+      setrows(data);
+  }
+  useEffect(() => {
+    setdata();
+  }, []);
+  let num = 0;
+  return(
+    <div>
+      <Title>Recent Orders</Title>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Ship To</TableCell>
+            <TableCell>Payment Method</TableCell>
+            <TableCell align="right">Sale Amount</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row._id}>
+              <TableCell>{row.createdAt}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.shipTo}</TableCell>
+              <TableCell>{row.paymentMethod}</TableCell>
+              <TableCell align="right">{row.amount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className={classes.seeMore}>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          See more orders
+        </Link>
+      </div>
+    </div>
+  );
+}
